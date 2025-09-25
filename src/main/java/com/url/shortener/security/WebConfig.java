53b3e27b843655a32d06b6150d2ad9af5f4b2e69
Abhay-Agnihotri01 <techsci0110@gmail.com>
2025-09-25 16:39:26 +0530

    sixth commit

--- src/main/java/com/url/shortener/security/WebConfig.java
+++ src/main/java/com/url/shortener/security/WebConfig.java
@@ -1,23 +1,23 @@
-package com.url.shortener.security;
+// package com.url.shortener.security;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${frontend.url}")
-    String frontEndUrl;
+//     @Value("${frontend.url}")
+//     String frontEndUrl;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(frontEndUrl)
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(false);
-    }
-}
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins(frontEndUrl)
+//                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(false);
+//     }
+// }
 
